c8eea42c15fad757897b8b4fc1b90e586c82cb2c
Faniloniaina RAMILISON <faniloniainaa21@gmail.com>
2024-02-24 23:45:10 +0300

    fix : fixing bug in route

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,8 +1,10 @@
 import express from "express"
-import { getAllUsers } from "../controllers/UserController"
+import { deleteUser, getAllUsers, registerUser, updateUser } from "../controllers/UserController"
 
 const router = express.Router()
 
 router.get('/get-all-user',getAllUsers)
-
+router.post('/create-user',registerUser)
+router.put('/update-user/:id',updateUser)
+router.delete("/delete-user/:id", deleteUser)
 export default router
